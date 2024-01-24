23f571c66790c5dbc33b5e3fe18957e2052e57d3
marcotulionasc <marconascpriv@gmail.com>
2024-01-24 11:50:29 -0300

    adicionado paymentWay (forma de pagamento)

---
+++ js/paymentWay.js
@@ -0,0 +1,9 @@
+function billingProfilePix() {
+    document.getElementById('pix-section').style.display = 'block';
+    document.getElementById('credito-section').style.display = 'none';
+}
+
+function billingProfileCredit() {
+    document.getElementById('pix-section').style.display = 'none';
+    document.getElementById('credito-section').style.display = 'block';
+}
